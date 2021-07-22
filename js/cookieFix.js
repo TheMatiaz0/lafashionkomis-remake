c88b0b00e40f79a8a258525eb646c23f9a022295
TheMatiaz0 <TheMatiaz0@protonmail.com>
2021-07-22 23:28:05 +0200

    Adding script for day of week and wip calculus for system in home page + home page close to finish (added Google Maps embed map, contact section and more text)

---
+++ js/cookieFix.js
@@ -0,0 +1,7 @@
+// document.cookie = 'cookie2=value2; SameSite=None; Secure';
+// alert(document.cookie);
+
+$(document).ready(function()
+{
+    // initialize();
+});
--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 var timer = null;
+var checker = $("#checker")
 
-$("#checker").mouseenter(function()
+var days = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
+var workHours = ['25 -1', '11 18', '11 18', '11 18', '11 18', '11 18', '10 14'];
+
+checker.mouseenter(function()
 {
     start();
 });
 
-$("#checker").mouseleave(function()
+checker.mouseleave(function()
 {
     clr();
 });
@@ -14,8 +18,42 @@ function changer()
 {
     var dt = new Date();
     var time = dt.getHours() + ":" + dt.getMinutes();
+    var adder = dt.getDay();
+    var dayOfWeek = days[adder];
+    if (dt.getDay() == 0)
+    {
+        adder = dt.getDay() + 1;
+    }
+    var hours = workHours[adder];
+    var startHour = hours[0];
+    var endHour = hours[1];
 
+    var finalText = `Na bazie twojego lokalnego czasu, jest teraz ${dayOfWeek.toLowerCase()}, godzina ${time}.<br />`
+
+    /*
+    if (dt.getHours() > startHour)
+    {
+
+        //good
+    }
+
+    else if (dt.getHours < startHour)
+    {
+        finalText += `To oznacza, że komis zostanie otwarty za ${abs(dt.getHours() - startHour)}`;
+        alert("k")
+        // too before :D
+    }
+
+    else if (dt.getHours == startHour)
+    {
+        // open
+    }
+    */
     
+    checker.attr("data-bs-original-title", finalText);
+    const tooltipElement = document.querySelector('[data-bs-toggle="tooltip"]');
+    let tool = bootstrap.Tooltip.getInstance(tooltipElement);
+    tool.show();
 }
 
 function start()
